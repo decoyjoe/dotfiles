96fd302c8d828fcc50757009f9672594b5a8f5d3
Joseph Larionov <decoyjoe@gmail.com>
2024-11-21 21:13:28 -0800

    Found why keychain wasn't working in WSL2

--- .shellrc/shell.d/keychain.sh
+++ .shellrc/shell.d/keychain.sh
@@ -1,2 +1,5 @@
 
+# If you're running WSL2 and keychain isn't working (i.e. ssh-add can't find the socket file)
+# it may be due to this WSL2 bug: https://github.com/microsoft/WSL/issues/11857
+
 eval $(keychain --eval --noask --quiet)
