9ebee2032227c745be8c9e3d80dc84492ef97727
Joseph Larionov <joseph@larionov.io>
2025-10-08 11:01:45 -0700

    Add oh-my-posh prompt to Bash

--- .shellrc/shell.d/10-environment.sh
+++ .shellrc/shell.d/10-environment.sh
@@ -1,2 +1,15 @@
 
 export EDITOR=vim
+
+# Add Homebrew path only on macOS
+if [ "$(uname)" = "Darwin" ]; then
+  # Add /opt/homebrew/bin to the PATH if it exists and isn't already there
+  if [ -d /opt/homebrew/bin ]; then
+    case ":$PATH:" in
+      *:/opt/homebrew/bin:*) : ;;  # already in PATH
+      *) PATH="/opt/homebrew/bin:$PATH" ;;
+    esac
+  fi
+
+  export PATH
+fi
