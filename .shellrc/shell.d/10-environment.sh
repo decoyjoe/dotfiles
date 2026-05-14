fd9c291a08bdca9ecf467fd955334e9d314a86e2
Joseph Larionov <joseph@larionov.io>
2026-05-14 11:29:17 -0700

    Enable support for fnm (Node.js version manager)

--- .shellrc/shell.d/10-environment.sh
+++ .shellrc/shell.d/10-environment.sh
@@ -1,4 +1,14 @@
 
+current_shell=''
+
+if [ -n "$ZSH_VERSION" ]; then
+  current_shell='zsh'
+fi
+
+if [ -n "$BASH_VERSION" ]; then
+  current_shell='bash'
+fi
+
 # Add Homebrew path only on macOS
 if [ "$(uname)" = "Darwin" ]; then
   # Add /opt/homebrew/bin to the PATH if it exists and isn't already there
@@ -15,6 +25,7 @@ if [ "$(uname)" = "Darwin" ]; then
   # How /etc/paths works: https://unix.stackexchange.com/a/356720
 
   # Define a bash alias only if the current bash isn't from Homebrew
+  # This is only for macOS because it ships a really old version of Bash
   current_bash="$(command -v bash 2>/dev/null || true)"
   homebrew_bash="/opt/homebrew/bin/bash"
 
@@ -22,3 +33,12 @@ if [ "$(uname)" = "Darwin" ]; then
     alias bash="${homebrew_bash}"
   fi
 fi
+
+if command -v fnm &>/dev/null; then
+  if [ -n "$current_shell" ]; then
+    eval "$(fnm env --use-on-cd --shell ${current_shell})"
+  else
+    eval "$(fnm env --use-on-cd)"
+  fi
+fi
+
