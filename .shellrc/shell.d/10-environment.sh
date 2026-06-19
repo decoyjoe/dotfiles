155feccf9e4e628a7dc5be3fb6aa6693cf68b581
Joseph Larionov <joseph@larionov.io>
2026-06-19 09:42:54 -0700

    [shellrc] Create my own ssh menu with fzf and ssh history

--- .shellrc/shell.d/10-environment.sh
+++ .shellrc/shell.d/10-environment.sh
@@ -43,3 +43,12 @@ if command -v fnm &>/dev/null; then
   fi
 fi
 
+# fzf: https://github.com/junegunn/fzf
+if command -v fzf &>/dev/null; then
+  # Dracula theme
+  export FZF_DEFAULT_OPTS=$'--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9,fg+:#f8f8f2,bg+:#44475a
+    --color=hl+:#bd93f9,info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
+    --color=marker:#ff9ed4,spinner:#ffb86c,header:#6272a4,border:#6272a4
+    --color=gutter:#44475a'
+fi
+
--- .shellrc/shell.d/20-alias.sh
+++ .shellrc/shell.d/20-alias.sh
@@ -4,4 +4,21 @@ alias ll='ls -l'
 alias la='ls -la'
 alias lg='lazygit'
 alias psh='pwsh.exe'
-alias s='sshs -e'
+
+# Fuzzy-find ssh commands from history.
+s() {
+  local cmd
+  cmd=$(history | grep -E '^\s*[0-9]+\s+ssh\s' | sed 's/^[ ]*[0-9]*[ ]*//' | sort -u | fzf --height=~40% --prompt="ssh> ")
+
+  if [ -n "$cmd" ]; then
+    # Inject into history so after exiting ssh, up-arrow recalls the actual ssh command (not "s").
+    if [ -n "$ZSH_VERSION" ]; then
+      print -s "$cmd"
+    else
+      history -s "$cmd"
+    fi
+
+    eval "$cmd"
+  fi
+}
+
